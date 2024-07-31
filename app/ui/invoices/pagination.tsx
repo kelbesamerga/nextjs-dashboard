ece65f5ade8d7e788bed5b9dddc8e58c6abb0383
Kelbesa Merga <kelbesa.m@zamzamBank.local>
2024-07-31 10:37:19 +0300

    CRUD operation of invoices

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -16,11 +16,13 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
     params.set('page', pageNumber.toString());
     return `${pathname}?${params.toString()}`;
   };
+    const allPages = generatePagination(currentPage, totalPages); // Assuming generatePagination is a function that returns an array of pages
+
   return (
     <>
       {/*  NOTE: Uncomment this code in Chapter 11 */}
 
-      {/* <div className="inline-flex">
+      <div className="inline-flex">
         <PaginationArrow
           direction="left"
           href={createPageURL(currentPage - 1)}
@@ -53,7 +55,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
           href={createPageURL(currentPage + 1)}
           isDisabled={currentPage >= totalPages}
         />
-      </div> */}
+      </div>
     </>
   );
 }
